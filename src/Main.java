a5ec7ddae54f04c98bd179b5df19acac0b056616
KitijaC <cietvirakitija@gmail.com>
2023-03-06 18:57:32 +0200

    data types updated

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        // 180 it's number literal (just value)
-        System.out.println(180);
+        // 100 it's number literal (just value)
+        System.out.println(10000000);
 
         // variable:
         // holder for data
@@ -10,11 +10,10 @@ public class Main {
         // needs type of variable
         // that data type limit possible set of allowed values
         // int - integer numbers
-
         int myDaughterAge = 5;
 
         // assigning new value to variable
-        myDaughterAge = 7;
+        myDaughterAge = 6;
         // sout + enter
         System.out.println(myDaughterAge);
 
@@ -22,7 +21,7 @@ public class Main {
         // String type is for words and letters
         String myName = "Kitija";
         String mySurname = "Cietvira";
-        System.out.println(myName + " " + mySurname);
+        System.out.println(myName + "-" + mySurname);
 
         String five = "5";
         String otherFive = "5";
@@ -35,10 +34,22 @@ public class Main {
         System.out.println(trueValue);
 
         // 11.0
-        int eleven = 11.0;
+        int eleven = 11;
 
         // double type for number with fraction part
         double trueEleven = 11.0;
 
+        System.out.println("Exercise 7");
+        int sum = 5 + 8;
+        System.out.println("Sum of 5 and 8 is = " + sum);
+
+        int difference = 11 - 4;
+        System.out.println("Difference of 11 and 4 is: " + difference);
+
+        int product = 5 * 8;
+        System.out.println("Product of 5 and 8 is: " + product);
+
+        double quotient = 5.0/8.5;
+        System.out.println("Quotient of 5.0 and 8.5 is: " + quotient);
     }
 }

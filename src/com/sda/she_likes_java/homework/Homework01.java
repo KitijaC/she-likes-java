6f044e1c4d46274febb69109a012c39c00247282
KitijaC <cietvirakitija@gmail.com>
2023-03-09 11:23:13 +0200

    Adding all homeworks

---
+++ src/com/sda/she_likes_java/homework/Homework01.java
@@ -0,0 +1,40 @@
+package com.sda.she_likes_java.homework;
+
+//Exercise 1
+//Write a programme that writes a Christmas tree.
+//Use a star (*) to draw it.
+//The Christmas tree must have at least 5 lines.
+
+public class Homework01 {
+    public static void main(String[] args) {
+        System.out.println("      *");
+        System.out.println("     ***");
+        System.out.println("   *******");
+        System.out.println("     ***          *");
+        System.out.println("   *******       ***");
+        System.out.println(" ***********    *****");
+        System.out.println("     **        *******");
+        System.out.println("     **          **");
+    }
+}
+
+//Exercise 2.
+//Try different errors in the application code:
+//- unclosed brackets
+//- no semicolon at the end of a line
+//- check how the compiler informs you of these errors.
+//
+//
+//Unclosed brackets:
+//
+//java: ')' expected
+//
+//
+//No semicolon at the end of a line:
+//
+//java: ';' expected
+//
+//
+//Check how the compiler informs you of these errors:
+//
+//With a red note in the terminal
---
+++ src/com/sda/she_likes_java/homework/Homework03.java
@@ -0,0 +1,24 @@
+package com.sda.she_likes_java.homework;
+//Exercise 7.
+//Create a simple calculator:
+//- definition of four variables which you will write out at the end
+//- sum
+//- difference
+//- product
+//- quotient
+//- consider which data types would be best
+public class Homework03 {
+    public static void main(String[] args) {
+        System.out.println("Simple calculator:");
+
+        int sum = 5 + 7;
+        int difference = 20 - 13;
+        int product = 3 * 9;
+        int quotient = 14 / 7;
+
+        System.out.println("5 + 7 = " + sum);
+        System.out.println("20 - 13 = " + difference);
+        System.out.println("3 * 9 = " + product);
+        System.out.println("15 / 7 = " +quotient);
+    }
+}

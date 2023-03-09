ec40c09019ca295ae9fab08b7ac506e684820653
KitijaC <cietvirakitija@gmail.com>
2023-03-09 12:46:59 +0200

    Homework05 - conditionals

---
+++ src/com/sda/she_likes_java/homework/BouncerInDisco.java
@@ -0,0 +1,28 @@
+package com.sda.she_likes_java.homework;
+
+import java.util.Scanner;
+
+public class BouncerInDisco {
+    public static void main(String[] args) {
+
+        int usersAge;
+        int usersHeight;
+
+        Scanner inputReader = new Scanner(System.in);
+
+        System.out.println("Enter your age: ");
+        usersAge = inputReader.nextInt();
+        System.out.println("Your age is: " + usersAge);
+
+        System.out.println("Enter your height: ");
+        usersHeight = inputReader.nextInt();
+        System.out.println("Your height is: " + usersHeight + "cm");
+
+        if ((usersAge >= 16) && (usersHeight >= 160)) {
+            System.out.println("You can enter the disco!");
+        } else {
+            System.out.println("Sorry, you can't enter the disco!");
+        }
+
+    }
+}

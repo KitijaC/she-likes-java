797700484aec842ff7a7bd66997ca8db3d486e7e
KitijaC <cietvirakitija@gmail.com>
2023-03-11 16:27:23 +0200

    Homework - exercise 10

---
+++ src/com/sda/she_likes_java/homework/BouncersInDiscoStory.java
@@ -0,0 +1,40 @@
+//Exercise 10.
+//A story about bouncers in a disco
+//Prepare a new class BouncersInDiscoStory and in it
+//- get the user’s age, ability to sing well and height from the console
+//- there are two bouncers in the disco: young and old
+//- Young checks if the person is adult, tall (or short :)) and can sing well
+//- Old has some kind of sight problem - so if any of the conditions are met, the person can enter the disco
+//- people try to cheat, so if someone is rejected by the young bouncer they try again with the old one:)
+
+package com.sda.she_likes_java.homework;
+
+import java.util.Scanner;
+
+public class BouncersInDiscoStory {
+    public static void main(String[] args) {
+        System.out.println("It's time to party!");
+
+        String declineMessage = "Sorry, you can't attend the party. Maybe next time!";
+        String canEnterDiscoMessage = "Congrats! You can enter the party!";
+
+        System.out.println("What is your age?");
+        Scanner inputReader = new Scanner(System.in);
+        int age = inputReader.nextInt();
+        System.out.println("What is your height?");
+        int height = inputReader.nextInt();
+        System.out.println("Is it true or false that you can sing well?");
+        boolean canSing = inputReader.nextBoolean();
+
+        boolean youngBouncer = age >= 18 && height >= 160 && canSing;
+        boolean oldBouncer = age >= 18 || height >= 160 || canSing;
+
+        if (youngBouncer) {
+            System.out.println(canEnterDiscoMessage);
+        } else if (oldBouncer) {
+            System.out.println(canEnterDiscoMessage);
+        } else {
+            System.out.println(declineMessage);
+        }
+    }
+}

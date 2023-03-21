88e9346866cc03c51996f2c496f709c19066a1e8
KitijaC <cietvirakitija@gmail.com>
2023-03-21 18:56:33 +0200

    Homework - Exercise 12 - updated

--- src/com/sda/she_likes_java/homework/Homework09.java
+++ src/com/sda/she_likes_java/homework/Homework09.java
@@ -4,41 +4,24 @@
 
 package com.sda.she_likes_java.homework;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Homework09 {
     public static void main(String[] args) {
 
-        //Bubble sort?
-
         Random numberGenerator = new Random();
         int firstRandomNumber = numberGenerator.nextInt(100);
         int secondRandomNumber = numberGenerator.nextInt(100);
         int thirdRandomNumber = numberGenerator.nextInt(100);
-        int temp;
 
         System.out.println("Three random numbers from 1 to 100 are: "
                 + firstRandomNumber + "; " + secondRandomNumber + "; " + thirdRandomNumber);
 
-        if (secondRandomNumber < firstRandomNumber) {
-            temp = firstRandomNumber;
-            firstRandomNumber = secondRandomNumber;
-            secondRandomNumber = temp;
-        }
-
-        if (thirdRandomNumber < secondRandomNumber) {
-            temp = secondRandomNumber;
-            secondRandomNumber = thirdRandomNumber;
-            thirdRandomNumber = temp;
-        }
-
-        if (secondRandomNumber < firstRandomNumber) {
-            temp = firstRandomNumber;
-            firstRandomNumber = secondRandomNumber;
-            secondRandomNumber = temp;
-        }
+        int[] numbers = {firstRandomNumber, secondRandomNumber, thirdRandomNumber};
+        Arrays.sort(numbers);
 
         System.out.println("These numbers in ascending order: "
-                + firstRandomNumber + "; " + secondRandomNumber + "; " + thirdRandomNumber);
+                + numbers[0] + "; " + numbers[1] + "; " + numbers[2]);
     }
 }

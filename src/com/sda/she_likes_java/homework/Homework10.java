5094631a625bd5b6206552e6ebdbe6c91685a12f
KitijaC <cietvirakitija@gmail.com>
2023-03-21 14:18:58 +0200

    Homework - Exercise 13

---
+++ src/com/sda/she_likes_java/homework/Homework10.java
@@ -0,0 +1,22 @@
+//Exercise 13
+//Create an int array with 100 elements
+//Initialise each element with a value 3 times greater than the current index
+//e.g.
+//tab[0] = 3 * 0
+//tab[10] = 3 * 10.
+
+package com.sda.she_likes_java.homework;
+
+public class Homework10 {
+    public static void main(String[] args) {
+        int[] numbers = new int[100];
+
+        for (int index = 0; index < numbers.length; index++) {
+            numbers[index] = index * 3;
+        }
+
+        for (int index = 0; index < numbers.length; index++) {
+            System.out.println("Number * 3 on index: " + index + " is " + numbers[index]);
+        }
+    }
+}

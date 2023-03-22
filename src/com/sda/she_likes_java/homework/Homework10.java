145a33f7f29e66599a07012b1e120277832dab4b
KitijaC <cietvirakitija@gmail.com>
2023-03-22 19:22:00 +0200

    Homework - Exercise 12 - added comments

--- src/com/sda/she_likes_java/homework/Homework10.java
+++ src/com/sda/she_likes_java/homework/Homework10.java
@@ -9,12 +9,18 @@ package com.sda.she_likes_java.homework;
 
 public class Homework10 {
     public static void main(String[] args) {
+        //Create an int array with 100 elements
         int[] numbers = new int[100];
 
+        //initialization phase
+        //first index is: 0
+        //last index is: number.length - 1
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = index * 3;
+            //numbers[i -1]
         }
 
+        //read data
         for (int index = 0; index < numbers.length; index++) {
             System.out.println("Number * 3 on index: " + index + " is " + numbers[index]);
         }

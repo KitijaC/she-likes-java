ca5371f4e4b4838bc698606b793fd652705c7cc7
KitijaC <cietvirakitija@gmail.com>
2023-03-24 18:32:36 +0200

    Homework 11 - exercise 14

--- src/com/sda/she_likes_java/homework/H_work_11/Address.java
+++ src/com/sda/she_likes_java/homework/H_work_11/Address.java
@@ -15,5 +15,23 @@ public class Address {
     String city;
     String street;
     int postalCode;
+    public static void main(String[] args) {
+
+        Address school = new Address();
+        school.country = "Latvia";
+        school.city = "Talsi";
+        school.street = "street Skolas 3";
+        school.postalCode = 3261;
+
+        Address hospital = new Address();
+        hospital.country = "Latvia";
+        hospital.city = "Ventspils";
+        hospital.street = "street Saules 12";
+        hospital.postalCode = 3220;
+
+        System.out.println("My school address is: " + school.country + ", " + school.city + ", " + school.street + ", LV-" + school.postalCode);
+        System.out.println("Address of the nearest hospital is: " + hospital.country + ", " + hospital.city + ", " + hospital.street + ", LV-" + hospital.postalCode);
+
+    }
 
 }

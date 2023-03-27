cb6a62524aa89447ea35616cb56c4b339a9a0e98
KitijaC <cietvirakitija@gmail.com>
2023-03-27 21:11:06 +0300

    static method first touch

--- src/com/sda/she_likes_java/classes/constructor/FullPerson.java
+++ src/com/sda/she_likes_java/classes/constructor/FullPerson.java
@@ -26,9 +26,24 @@ public class FullPerson {
                 '}';
     }
 
+    public void describeYourself() {
+        System.out.println("Hi, I am " + name);
+    }
+
+    public static void genericDescription() {
+        System.out.println("Generic description");
+    }
+
     public static void main(String[] args) {
         FullAddress address = new FullAddress("Italy", "Rome", "La Lafa", "002-4");
         FullPerson person = new FullPerson("Ann", "Green", 22, address);
         System.out.println("Person is: " + person);
+
+        person.describeYourself();
+
+        FullPerson personWithoutAddress = new FullPerson("Elizabeth", "Stone", 16, null);
+        personWithoutAddress.describeYourself();
+
+        genericDescription();
     }
 }

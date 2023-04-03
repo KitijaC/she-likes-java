d6567d79c023fd4147f876405cd7f255ffacbf51
KitijaC <cietvirakitija@gmail.com>
2023-04-03 17:38:44 +0300

    House example with new references

--- src/com/sda/she_likes_java/objects/House.java
+++ src/com/sda/she_likes_java/objects/House.java
@@ -3,16 +3,8 @@ package com.sda.she_likes_java.objects;
 public class House {
     private int numberOfRooms;
     private int numberOfBathrooms;
-
+    
     private int numberOfFloors;
-
-    public int getNumberOfFloors() {
-        return numberOfFloors;
-    }
-
-    public void setNumberOfFloors(int numberOfFloors) {
-        this.numberOfFloors = numberOfFloors;
-    }
 
     public int getNumberOfRooms() {
         return numberOfRooms;
@@ -30,12 +22,6 @@ public class House {
         this.numberOfBathrooms = numberOfBathrooms;
     }
 
-    public House(int numberOfRooms, int numberOfBathrooms, int numberOfFloors) {
-        this.numberOfRooms = numberOfRooms;
-        this.numberOfBathrooms = numberOfBathrooms;
-        this.numberOfFloors = numberOfFloors;
-    }
-
     public House(int numberOfRooms, int numberOfBathrooms) {
         this.numberOfRooms = numberOfRooms;
         this.numberOfBathrooms = numberOfBathrooms;
@@ -47,5 +33,8 @@ public class House {
                 "numberOfRooms=" + numberOfRooms +
                 ", numberOfBathrooms=" + numberOfBathrooms +
                 '}';
+    }
+
+    public void setNumberOfFloors(int i) {
     }
 }
--- src/com/sda/she_likes_java/objects/HouseExample.java
+++ src/com/sda/she_likes_java/objects/HouseExample.java
@@ -2,45 +2,43 @@ package com.sda.she_likes_java.objects;
 
 public class HouseExample {
     public static void main(String[] args) {
-        House myHouse = new House(4,2);
+        House myHouse = new House(4, 2);
         myHouse.setNumberOfFloors(2);
-        System.out.println("I've got a new house: " + myHouse);
-
+        System.out.println("I've got new house: " + myHouse);
+        // stage 1
         System.out.println("Currently my house has got " + myHouse.getNumberOfRooms() + " rooms");
         System.out.println("I need more rooms...");
         myHouse.setNumberOfRooms(5);
-
+        // stage 2
         System.out.println("Hey, I've got more rooms now...");
         System.out.println("Currently my house has got " + myHouse.getNumberOfRooms() + " rooms");
-
-        System.out.println("=================================");
+        System.out.println("=======================================");
         HouseOwner wife = new HouseOwner("Mary", myHouse);
+        // stage 3
         HouseOwner husband = new HouseOwner("Jack", myHouse);
-
+        // stage 4
         System.out.println("Mary would like to repair her house");
         wife.getHouse().setNumberOfBathrooms(3); // House house = wife.getHouse(); house.setNumberOfBathrooms(3);
         System.out.println("Currently the house looks like this: " + myHouse);
-
+        // stage 5
         husband.getHouse().setNumberOfBathrooms(1);
         System.out.println("Currently the house looks like this: " + myHouse);
-
+        // stage 6
         System.out.println("Mary: " + wife);
         System.out.println("Jack: " + husband);
-
-//        String haha = new House(1,2);
-        myHouse = new House(1,1); //this house not existing anymore because of 35 line
-        System.out.println("My house reference: " + myHouse);
+//        String haha = new House(1, 2);
+        myHouse = new House(1, 1); ////this house not existing anymore because of 35 line
+        System.out.println("Now myHouse reference: " + myHouse);
         System.out.println("Mary: " + wife);
         System.out.println("Jack: " + husband);
-
-        myHouse = new House(10,10); // reference to a new house
+        // stage 7
+        myHouse = new House(10, 10); // reference to a new house
         wife.setHouse(myHouse); // wife moves to the new house
+        // stage 8
         System.out.println("Mary: " + wife);
         System.out.println("Jack: " + husband);
-
         wife.setHouse(husband.getHouse()); // wife moves back to husband. Husband is the reference of that previous house
-        System.out.println("Mary: " + wife);
-        System.out.println("Jack: " + husband);
+        // stage 9
 
     }
 }

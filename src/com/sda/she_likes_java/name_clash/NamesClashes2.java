0e2882591fd8c83e31dfa342a05d40af06826898
KitijaC <cietvirakitija@gmail.com>
2023-04-19 19:27:42 +0300

    Date class - solution how to refer to different classes with the same simple name

---
+++ src/com/sda/she_likes_java/name_clash/NamesClashes2.java
@@ -0,0 +1,18 @@
+package com.sda.she_likes_java.name_clash;
+
+public class NamesClashes2 {
+    public static void main(String[] args) {
+
+        // referring our own Date class - the best solution
+        Date myDate = new Date();
+
+        // java.sql.Date - canonical class name
+        java.sql.Date sqlDate = new java.sql.Date(100);
+
+        // the same as 6 line
+        com.sda.she_likes_java.name_clash.Date myDate2 = new com.sda.she_likes_java.name_clash.Date();
+
+        // java.utilDate
+        java.util.Date utilDate = new java.util.Date(1000);
+    }
+}

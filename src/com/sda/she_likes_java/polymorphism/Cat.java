46b07b6e04e868bea06dac669d76ca5082ad891c
KitijaC <cietvirakitija@gmail.com>
2023-04-21 21:44:41 +0300

    Inheritance

---
+++ src/com/sda/she_likes_java/polymorphism/Cat.java
@@ -0,0 +1,14 @@
+package com.sda.she_likes_java.polymorphism;
+
+public class Cat
+        extends Animal
+{
+    @Override
+    public void makeSound() {
+        System.out.println("Mjau");
+    }
+
+    public void sleep() {
+        System.out.println("Cat is sleeping");
+    }
+}

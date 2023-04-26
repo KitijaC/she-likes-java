d570e91ea11fb439d5287fad20b829fd9bf93114
KitijaC <cietvirakitija@gmail.com>
2023-04-26 19:06:20 +0300

    Homework 18, exercise 20 - Mariusz example

--- src/com/sda/she_likes_java/other/exercise_20/Car.java
+++ src/com/sda/she_likes_java/other/exercise_20/Car.java
@@ -1,4 +1,20 @@
+//Exercise 20
+//Create a Car class with a drive() method.
+//Create 3 subclasses:
+//- Mazda
+//- Toyota
+//- Ferrari
+//Override the drive() method in each of the subclasses.
+//In addition, create three unique methods in each subclass.
+//Create a class CarUtils with method safeCast(Car car)
+//- inside it, try a safe cast for Mazda and Toyota to call specific methods.
+//- if the object sent is of a different type - just output the information to the console.
+//- have fun
+
 package com.sda.she_likes_java.other.exercise_20;
 
 public class Car {
+    public void drive() {
+        System.out.println("Car is driving");
+    }
 }
---
+++ src/com/sda/she_likes_java/other/exercise_20/CarUtils.java
@@ -0,0 +1,13 @@
+package com.sda.she_likes_java.other.exercise_20;
+
+public class CarUtils {
+    public static void safeCast(Car car) {
+        if (car instanceof Mazda) {
+            System.out.println("Is Mazda nice: " + ((Mazda) car).isNice());
+        } else if (car instanceof Toyota) {
+            System.out.println("Is Toyota safe: " + ((Toyota) car).isSafe());
+        } else {
+            System.out.println("Different type of car");
+        }
+    }
+}

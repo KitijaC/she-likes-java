a8f9c5373b3fa1fe9aa3a695b42531b888a30757
KitijaC <cietvirakitija@gmail.com>
2023-04-26 19:15:15 +0300

    Homework 18, exercise 20 - Mariusz example - added comment

--- src/com/sda/she_likes_java/other/exercise_20/CarUtils.java
+++ src/com/sda/she_likes_java/other/exercise_20/CarUtils.java
@@ -1,7 +1,7 @@
 package com.sda.she_likes_java.other.exercise_20;
 
 public class CarUtils {
-    public static void safeCast(Car car) {
+    public static void safeCast(Car car) { // without "static"word we need to create in the Main class new objects CarUtil()
         if (car instanceof Mazda) {
             System.out.println("Is Mazda nice: " + ((Mazda) car).isNice());
         } else if (car instanceof Toyota) {

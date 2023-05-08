9a828d2882541b33cf9bd18da06210c2408e30f7
KitijaC <cietvirakitija@gmail.com>
2023-05-08 20:23:11 +0300

    lists example - ordered - can contain duplicates (according to equal() method)

---
+++ src/com/sda/she_likes_java/collections/lists/Main.java
@@ -0,0 +1,58 @@
+package com.sda.she_likes_java.collections.lists;
+
+import com.sda.she_likes_java.collections.Trousers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 5;
+        a = 6;
+
+        final int b = 7; // final value, can't change it after
+//        b = 8; // this is not working
+
+        // expandable list
+        List<Trousers> trousers = new ArrayList<>(); // we use lists if we care about the order of items
+        trousers.add(new Trousers("jeans", "big star"));
+        trousers.add(new Trousers("jeans", "primark"));
+
+        showItemsFromTheListList(trousers);
+        //let's add another item to the list
+        trousers.add(new Trousers("jeans", "primark"));
+
+        // frozen list
+        List<Trousers> trousersList = List.of( //limited number of items, cant add more of items
+                new Trousers("jeans", "big star"),
+                new Trousers("jeans", "big star"),
+                new Trousers("jeans", "big star")
+        );
+
+        showItemsFromTheListList(trousersList);
+//        trousersList.add(new Trousers("jeans", "primark")); // can't do this
+
+        List<Trousers> trousersList2 = List.of( // completely different list of items
+                new Trousers("jeans", "big star"),
+                new Trousers("jeans", "big star"),
+                new Trousers("jeans", "big star"),
+                new Trousers("jeans", "primark"),
+                new Trousers("jeans", "primark"),
+                new Trousers("jeans", "primark")
+        );
+    }
+
+    public static void showItemsFromTheListList (List<Trousers> items) {
+        // enhanced for loop
+        for (Trousers currentItem: items) { // right side - collection, left side - in every iteration had every value from collection
+            System.out.println(currentItem);
+        }
+
+        // old loop
+        for (int i = 0; i < items.size(); i++) {
+            Trousers currentItem = items.get(i);
+            System.out.println(currentItem);
+        }
+    }
+}

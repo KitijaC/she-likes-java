4a55537ddad9e8a4662ef05a775e9c3de24ba6d8
KitijaC <cietvirakitija@gmail.com>
2023-05-08 21:31:31 +0300

    map example- iteration

--- src/com/sda/she_likes_java/collections/maps/Main.java
+++ src/com/sda/she_likes_java/collections/maps/Main.java
@@ -6,6 +6,10 @@ import com.sda.she_likes_java.collections.Trousers;
 import java.util.HashMap;
 import java.util.Map;
 
+//map example:
+//- contains values under the keys (value is represented by key)
+//- any type can be type of the key
+//- any type can be type of the value
 public class Main {
     public static void main(String[] args) {
         System.out.println("Maps are like dictionaries");
@@ -15,14 +19,25 @@ public class Main {
         // value: new Trousers("jeans", "big star")
         myJeans.put("my favorite jeans", new Trousers("jeans", "big star"));
         myJeans.put("my old ones", new Trousers("unknown material", "primark"));
-        myJeans.put("my old ones", new Trousers("unknown material", "primark II"));
+        myJeans.put("my old ones", new Trousers("unknown material", "primark II")); //this will be printed, because it overrides the same key
+        printMyJeans(myJeans);
 
         Map<Person, Trousers> personsTrousers = new HashMap<>();
         personsTrousers.put(new Person("mariusz", "p."), new Trousers ("polish material", "primark"));
         personsTrousers.put(new Person("karolina", "p."), new Trousers ("polish material", "primark"));
+        System.out.println("persons trousers: " + personsTrousers);
     }
-    //map example:
-    //- contains values under the keys (value is represented by key)
-    //- any type can be type of the key
-    //- any type can be type of the value
+
+    public static void printMyJeans(Map<String, Trousers> trousersMap) {
+        System.out.println("Let's start with known keys");
+        System.out.println("key=[my favorite jeans] " + trousersMap.get("my favorite jeans"));
+        System.out.println("key=[no key] " + trousersMap.get("no key"));
+
+        System.out.println("Let's iterate the map");
+        // Map.Entry represents key + value at once
+        for (Map.Entry<String, Trousers> item : trousersMap.entrySet()) {
+            System.out.println("key: " + item.getKey());
+            System.out.println("value: " + item.getValue());
+        }
+    }
 }

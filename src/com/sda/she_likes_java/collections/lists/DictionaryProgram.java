252762c1683d499fa2423c79ce0df88e56333a7d
KitijaC <cietvirakitija@gmail.com>
2023-05-12 21:17:47 +0300

    custom example with dictionaries - reading input from user

--- src/com/sda/she_likes_java/collections/lists/DictionaryProgram.java
+++ src/com/sda/she_likes_java/collections/lists/DictionaryProgram.java
@@ -1,9 +1,33 @@
 package com.sda.she_likes_java.collections.lists;
 
+import java.io.Console;
+import java.util.Scanner;
+
 public class DictionaryProgram {
     public static void main(String[] args) {
-        MyGreatDictionary englishCzechDictionary = MyDictionaries.createEnglishCzechDictionary();
-        englishCzechDictionary.translateFromSourceLanguageToDestiny("dog");
-        englishCzechDictionary.translateFromSourceLanguageToDestiny("house");
+
+        System.out.println("Hi, which dictionary you'd like to use");
+        System.out.println("type 1 for English - Czech");
+        System.out.println("type 2 for English - Polish");
+//        Console console = System.console();
+//        String userChoice = console.readLine();
+        Scanner inputReader = new Scanner(System.in);
+        String userChoice = inputReader.next();
+
+        MyGreatDictionary currentDictionary;
+        if (userChoice.equals("1")) {
+            currentDictionary = MyDictionaries.createEnglishCzechDictionary();
+            System.out.println("let's use English - Czech dictionary");
+        } else if (userChoice.equals("2")) {
+            currentDictionary = MyDictionaries.createEnglishPolishDictionary();
+            System.out.println("let's use English - Polish dictionary");
+        } else {
+            System.out.println("I don't know what you mean");
+            return;
+        }
+
+        System.out.println("Provide word to translate: ");
+        String wordFromTheUser = inputReader.next();
+        currentDictionary.translateFromSourceLanguageToDestiny(wordFromTheUser);
     }
 }

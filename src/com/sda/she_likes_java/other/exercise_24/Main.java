3c6b08baecdfe0762bff4f4dfa00f93f0a4d6927
KitijaC <cietvirakitija@gmail.com>
2023-05-13 18:58:42 +0300

    Example of homework Exercise 24

---
+++ src/com/sda/she_likes_java/other/exercise_24/Main.java
@@ -0,0 +1,12 @@
+package com.sda.she_likes_java.other.exercise_24;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Let's learn some Polish");
+
+        EnglishPolishDictionary englishPolishDictionary = new EnglishPolishDictionary();
+        englishPolishDictionary.translate("sky");
+        englishPolishDictionary.translate("school");
+        englishPolishDictionary.translate("dog");
+    }
+}

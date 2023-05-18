9b2c820e06a87588b4647ffbedd794480d52b439
KitijaC <cietvirakitija@gmail.com>
2023-05-18 21:32:26 +0300

    Exceptions

---
+++ src/com/sda/she_likes_java/exceptions/BankExample.java
@@ -0,0 +1,23 @@
+package com.sda.she_likes_java.exceptions;
+
+public class BankExample {
+    public static void main(String[] args) {
+        BankConnector connector = new BankConnector();
+//        int balance = connector.accountBalanceAlwaysSuccessful();
+//        if (balance > 0) {
+//            System.out.println("I've got money");
+//        } else {
+//            System.out.println("Debit");
+//        } // we cant tell if there was error, so better way is with exceptions
+        System.out.println("Let's try to check balance of my account");
+        int myMoney = 0;
+        try {
+            myMoney = connector.accountBalance();
+            System.out.println("My money: " + myMoney);
+        } catch (Exception e) {
+            System.out.println("Unexpected condition occurred");
+            System.out.println("Exception: " + e);
+        }
+        System.out.println("Here");
+    }
+}

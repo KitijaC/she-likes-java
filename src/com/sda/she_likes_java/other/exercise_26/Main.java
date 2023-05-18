812728e831352d6ee07790f8c3b8814474d8518d
KitijaC <cietvirakitija@gmail.com>
2023-05-18 14:42:33 +0300

    Example of homework - exercise 26

---
+++ src/com/sda/she_likes_java/other/exercise_26/Main.java
@@ -0,0 +1,12 @@
+package com.sda.she_likes_java.other.exercise_26;
+
+public class Main {
+    public static void main(String[] args) {
+        ImportantEvents[] allImportantEvents = ImportantEvents.values();
+
+        for (ImportantEvents event : allImportantEvents) {
+            System.out.println("Important event: ");
+            System.out.println("\t" + event.getEventDate() + ": " + event.getDescription());
+        }
+    }
+}

449ecfec0ec26fcbb404b652a23332d18fdc5653
KitijaC <cietvirakitija@gmail.com>
2023-05-26 21:33:56 +0300

    Application for getting data from Test- almost working

--- src/com/sda/she_likes_java/database/DatabaseUtils.java
+++ src/com/sda/she_likes_java/database/DatabaseUtils.java
@@ -6,12 +6,38 @@ import java.sql.SQLException;
 
 public class DatabaseUtils {
 
+    /**
+     * script for creating table
+     * CREATE TABLE TEST
+     * (ID INT PRIMARY KEY, NAME VARCHAR(255));
+     *
+     * script for inserting one record (row) into table
+     * INSERT INTO
+     * TEST(ID, NAME)
+     * VALUES(1, 'Kitija');
+     *
+     * script for reading data
+     * SELECT ID, NAME
+     * FROM TEST;
+     *
+     * script for updating data
+     * UPDATE TEST -- name of table to update
+     * SET NAME = 'Alma' -- column value to change
+     * WHERE ID = 2; -- selected row
+     *
+     * script for deleting data
+     *
+     * DELETE
+     * FROM TEST
+     * WHERE ID=3;
+     */
+
     // handle internally
     public static Connection getConnection() {
         try {
-            return DriverManager.getConnection("jdbc:h2:tcp://localhost/~/databases/jdbc",
+            return DriverManager.getConnection("jdbc:h2:~\\Data\\she-goes-tech\\db",
                     "sa",
-                    "");
+                    "sa");
         } catch (SQLException e) {
             return null;
         }
@@ -19,8 +45,8 @@ public class DatabaseUtils {
 
     // declare
     public static Connection getConnectionButSometimesThrowException() throws SQLException {
-        return DriverManager.getConnection("jdbc:h2:tcp://localhost/~/databases/jdbc",
+        return DriverManager.getConnection("jdbc:h2:~\\Data\\she-goes-tech\\db",
                 "sa",
-                "");
+                "sa");
     }
 }

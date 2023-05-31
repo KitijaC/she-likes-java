380620700bdb296e1f6ff7a1267ba10d6a19f013
KitijaC <cietvirakitija@gmail.com>
2023-05-31 21:32:04 +0300

    read all students implemented

--- src/com/sda/she_likes_java/database_in_memory/DbInit.java
+++ src/com/sda/she_likes_java/database_in_memory/DbInit.java
@@ -9,19 +9,18 @@ public class DbInit {
     private static final String  createTableQuery = """
             CREATE TABLE STUDENTS
             (
-                ID INT PRIMARY KEY AUTO_INCREMENT,\s
-                NAME VARCHAR(255) NOT NULL,\s
-                SURNAME VARCHAR(255) NOT NULL,\s
-                AGE INT NOT NULL DEFAULT 0,\s
-                SEX VARCHAR(255));\s
+                ID INT PRIMARY KEY AUTO_INCREMENT,
+                NAME VARCHAR(255) NOT NULL,
+                SURNAME VARCHAR(255) NOT NULL,
+                AGE INT NOT NULL DEFAULT 0, SEX VARCHAR(255));
             """;
 
     private static final String insertDataQuery = """
-            INSERT INTO STUDENTS ( NAME, SURNAME, AGE, SEX) VALUES ('Mariss', 'Alvares', 23, 'MALE');
-            INSERT INTO STUDENTS ( NAME, SURNAME, AGE, SEX) VALUES ('Anita', 'Helen', 17, 'FEMALE');
-            INSERT INTO STUDENTS ( NAME, SURNAME, AGE, SEX) VALUES ('Elina', 'Sun', 15, 'FEMALE');
-            INSERT INTO STUDENTS ( NAME, SURNAME, AGE, SEX) VALUES ('Eva', 'Lewis', 55, 'FEMALE');
-            INSERT INTO STUDENTS ( NAME, SURNAME, AGE, SEX) VALUES ('Olaf', 'Wood', 10, 'MALE');                                                                                 
+            INSERT INTO STUDENTS (NAME, SURNAME, SEX) VALUES ('Olek', 'S.', 'MALE');
+            INSERT INTO STUDENTS (NAME, SURNAME, AGE, SEX) VALUES ('Marek', 'W.', 25, 'MALE');
+            INSERT INTO STUDENTS (NAME, SURNAME, AGE, SEX) VALUES ('Alina', 'G.', 18, 'FEMALE');
+            INSERT INTO STUDENTS (NAME, SURNAME, AGE, SEX) VALUES ('Anna', 'F.', 35, 'FEMALE');
+            INSERT INTO STUDENTS (NAME, SURNAME, AGE, SEX) VALUES ('EWELINA', 'A.', 40, 'FEMALE');                                                                                 
             """;
 
     public static void prepareData(Connection dbConnection) {

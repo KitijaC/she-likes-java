a0c07ef88f4db10e98859837662012ee401d0193
KitijaC <cietvirakitija@gmail.com>
2023-06-09 19:38:40 +0300

    Car related models created

--- src/com/sda/she_likes_java/final_project/entity/Car.java
+++ src/com/sda/she_likes_java/final_project/entity/Car.java
@@ -1,5 +1,16 @@
 package com.sda.she_likes_java.final_project.entity;
 
+import java.time.YearMonth;
+
 // TODO: add fields
-public record Car(Category category) {
+public record Car(
+        Long id,
+        String brand,
+        String model,
+        GearboxType gearboxType,
+        CarBody carBody,
+        PriceCategory category,
+        FuelType fuelType,
+        long mileage,
+        YearMonth productionDate) {
 }
